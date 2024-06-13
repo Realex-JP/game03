fe1590701aa75ca2a7964f76ae567cb98236b20a
Realex <1cpb3208@mail.u-tokai.ac.jp>
2024-06-14 07:55:57 +0900

    changed2

--- Tetris/tetris/Settings.java
+++ Tetris/tetris/Settings.java
@@ -9,7 +9,26 @@ public final class Settings {
     final static Dimension dim = new Dimension(700, 700); 
 
     final static Color[] colors = {Color.magenta, Color.pink, Color.red, Color.green, Color.blue, Color.cyan};
-    final static Color backgroundColor = Color.white;
+    final static Color backGroundColor = new Color(0xB3B8BB);
+    final static Color gridColor = new Color(0xBECFEA);
+    final static Color titleBackGroundColor = Color.white;
+    final static Color textColor = Color.black;
+    final static Color border = Color.white;
 
-    final static Font mainfont = new Font("Arial", Font.BOLD, 50);
+    final static int startX = 120;
+    final static int startY = 400;
+
+    final static int topSpace = 50;
+    final static int leftSpace = 20;
+    final static int blockSize = 30;
+
+    final static Stroke largeStroke = new BasicStroke(5);
+    final static Stroke smallStroke = new BasicStroke(2);
+
+    final static Rectangle titleRect = new Rectangle(100, 85, 252, 100);
+    final static Rectangle pressRect = new Rectangle(50, 375, 252, 40);
+    final static Rectangle gridRect = new Rectangle(46, 47, 308, 517);
+
+    final static Font mainFont = new Font("Arial", Font.BOLD, 50);
+    final static Font subFont = mainFont.deriveFont(Font.BOLD, 18);
 }
--- Tetris/tetris/Tetris.java
+++ Tetris/tetris/Tetris.java
@@ -61,11 +61,26 @@ public class Tetris extends JPanel implements Runnable {
         final int[][] pos, shape;
     }
 
+    public void startGame() {
+        stop();
+        grid();
+        choiceShape();
+        (fThread = new Thread(this)).start();
+    }
+
+    public void stop() {
+        if (fThread != null) {
+            Thread tmp = fThread;
+            fThread = null;
+            tmp.interrupt();
+        }
+    }
+
     public void grid() {
-        for(int g = 0; g < Row; g++) {
+        for (int g = 0; g < Row; g++) {
             Arrays.fill(grid[g], EMPTY);
-            for(int b = 0; b <Col; b++) {
-                if(b == 0 || b == Col - 1 || g == Row - 1)
+            for (int b = 0; b <Col; b++) {
+                if (b == 0 || b == Col - 1 || g == Row - 1)
                     grid[g][b] = BORDER;
             }
         }
@@ -79,9 +94,35 @@ public class Tetris extends JPanel implements Runnable {
         Shape[] shapes = Shape.values();
         nShape = shapes[rand.nextInt(shapes.length)];
 
-        if(fShape != null)
+        if (fShape != null)
             fShape.reset();
     }
+
+    public void startScreen(Graphics2D g) {
+        g.setColor(titleBackGroundColor);
+        g.setFont(mainFont);
+        g.fill(titleRect);
+        g.fill(pressRect);
+
+        g.setColor(textColor);
+        g.setFont(subFont);
+        g.drawString("Press any key to start", startX, startY);
+    }
+
+    public void drawSquare(Graphics2D g, int colorIndex, int r, int c) {
+        g.setColor(colors[colorIndex]);
+        g.fillRect(leftSpace + c * blockSize, topSpace + r * blockSize, blockSize, blockSize);  
+        g.setStroke(smallStroke);
+        g.setColor(border);
+        g.drawRect(leftSpace + c * blockSize, topSpace + r * blockSize, blockSize, blockSize);
+    }
+
+    public void drawFShape(Graphics2D g) {
+        int idx = fShape.ordinal();
+        for (int[] p : fShape.pos)
+            drawSquare(g, idx, fShapeRow + p[1], fShapeCol + p[0]);
+    }
+
 
     public Tetris() {
         grid();
@@ -89,7 +130,7 @@ public class Tetris extends JPanel implements Runnable {
 
         setFocusable(true);
         setPreferredSize(dim);
-        setBackground(backgroundColor);
+        setBackground(backGroundColor);
 
 
         addKeyListener(new KeyAdapter() {
@@ -107,16 +148,16 @@ public class Tetris extends JPanel implements Runnable {
                         break;
 
                     case KeyEvent.VK_RIGHT:
-                        if(movable(fShape))
+                        if (movable(fShape, Direction.right))
                             move(Direction.right);
 
                     case KeyEvent.VK_LEFT:
-                        if(movable(fShape))
+                        if (movable(fShape, Direction.left))
                             move(Direction.left);
                         break;
 
                     case KeyEvent.VK_DOWN:
-                        if(!hardDrop) {
+                        if (!hardDrop) {
                             hardDrop = true;
                             while (movable(fShape, Direction.down)) {
                                 move(Direction.down);
@@ -133,6 +174,143 @@ public class Tetris extends JPanel implements Runnable {
         });
     }
 
-    
+    @Override
+    public void run() {
+        while (Thread.currentThread() == fThread) {
+            try {
+                Thread.sleep(setSpeed());
+            } catch (InterruptedException e) {
+                return;
+            }
+
+            if (!gameOver()) {
+                if (movable(fShape, Direction.down)) {
+                    move(Direction.down);
+                } else {
+                    landed();
+                }
+                repaint();
+            }
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics gg) {
+        super.paintComponent(gg);
+        Graphics2D g = (Graphics2D) gg;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        if (!gameOver()) {
+            startScreen(g);
+        } else {
+            drawFShape(g);
+        }
+    }
+
+    public boolean rotatable(Shape s) {
+        if (s == Shape.OShape)
+            return false;
+
+        int[][] pos = new int[4][2];
+        for (int i = 0; i < pos.length; i++) {
+            pos[i] = s.pos[i].clone();
+        }
+
+        for (int[] row : pos) {
+            int tmp = row[0];
+            row[0] = row[1];
+            row[1] = -tmp;
+        }
+
+        for (int[] p : pos) {
+            int newCol = fShapeCol + p[0];
+            int newRow = fShapeRow + p[1];
+            if (grid[newRow][newCol] != EMPTY) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void rotate(Shape s) {
+        if (s == Shape.OShape)
+            return;
+
+        for (int[] row : s.pos) {
+            int tmp = row[0];
+            row[0] = row[1];
+            row[1] = -tmp;
+        }
+    }
+
+    public void move(Direction dir) {
+        fShapeRow += dir.y;
+        fShapeCol += dir.x;
+    }
+
+    public boolean movable(Shape s, Direction direction) {
+        for (int[] p : s.pos) {
+            int newCol = fShapeCol + direction.x + p[0];
+            int newRow = fShapeRow + direction.y + p[1];
+            if (grid[newRow][newCol] != EMPTY)
+                return false;
+        }
+        return true;
+    }
+
+    public void landed() {
+        addShape(fShape);
+        if (fShapeRow < 2) {
+            gameOver();
+            stop();
+        } else {
+            removeLines();
+        }
+        choiceShape();
+    }
+
+    public void addShape(Shape s) {
+        for (int[] p : s.pos)
+            grid[fShapeRow + p[1]][fShapeCol + p[0]] = s.ordinal();
+    }
+
+    public int removeLines() {
+        int count = 0;
+        for (int r = 0; r < Row - 1; r++) {
+            for (int c = 1; c < Col - 1; c++) {
+                if (grid[r][c] == EMPTY)
+                    break;
+                if (c == Col - 2) {
+                    count++;
+                    removeLine(r);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void removeLine(int line) {
+        for (int c = 0; c < Col; c++)
+            grid[line][c] = EMPTY;
+
+        for (int c = 0; c < Col; c++) {
+            for (int r = line; r > 0; r--)
+                grid[r][c] = grid[r - 1][c];
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setTitle("Tetris");
+            f.setResizable(false);
+            f.add(new Tetris(), BorderLayout.CENTER);
+            f.pack();
+            f.setLocationRelativeTo(null);
+            f.setVisible(true);
+        });
+    }
 
 }
